3def62f43927d3ab45fd70c4f774080b92c5720d
Denis Ciccale <dciccale@gmail.com>
2015-08-02 18:35:57 +0200

    return new stream

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-/*
- * Template file
- */
-
 'use strict';
 
 var exec = require('child_process').exec;
 var os = require('os');
 var _ = require('lodash');
+var Stream = require('stream');
 
 var format = require('./format');
 var sort = require('./sort');
 
 module.exports = function (opts, callback) {
+  var stream = new Stream;
   var defaults = {
     sort: 'chronological', // alphabetical, commits
     email: false, // Show emails in the output
     nomerges: false, // Only works when sorting by commits
   };
-
   var cmd = 'git';
+  var child;
 
   if (typeof opts === 'function') {
     callback = opts;
@@ -59,15 +57,24 @@ module.exports = function (opts, callback) {
     }
   }
 
-  return exec(cmd, function (err, stdout, stderr) {
+  child = exec(cmd, function (err, stdout, stderr) {
     callback = typeof callback === 'function' ? callback : console.log;
     if (err) {
+      stream.emit('data', stderr);
+      stream.emit('end', err);
       callback(err, stdout);
     } else {
       stdout = format(stdout);
       stdout = sort(options.sort, stdout);
       stdout = stdout.join(os.EOL);
+      stream.emit('data', stdout);
       callback(null, stdout);
     }
   });
+
+  child.on('close', function () {
+    stream.emit('end');
+  });
+
+  return stream;
 };
